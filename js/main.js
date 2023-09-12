820683d1c85db6875cff1b30d366d3596ad2ca0d
jmarc0s <joaomarcoscacula88@gmail.com>
2023-09-12 16:44:52 -0300

    removendo itens da lista

--- js/main.js
+++ js/main.js
@@ -1,23 +1,86 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+itens.forEach( (element) => {
+    criaElemento(element)
+});
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome']
+    const quantidade =  evento.target.elements['quantidade'];
+
+    const existe = itens.find(elemento => elemento.nome === nome.value)
+
+    const itemAtual = {
+        "nome": nome.value, 
+        "quantidade": quantidade.value
+    }
+
+
+    if (existe){
+        itemAtual.id = existe.id
+
+        atualizaElemento(itemAtual)
+
+        itens[existe.id] = itemAtual
+    }else{
+        itemAtual.id = itens.length
+        criaElemento(itemAtual)
+        itens.push(itemAtual)
+    }
+    
+    
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    
+
+    nome.value = ""
+    quantidade.value = ""
 })
 
-function criaElemento(nome, quantidade){
+function criaElemento(item){
     const novoItem = document.createElement('li')
     novoItem.classList.add('item')
 
     const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade 
+    numeroItem.innerHTML = item.quantidade 
+    numeroItem.dataset.id = item.id
 
 
     novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
+    novoItem.innerHTML += item.nome
+
+    novoItem.appendChild(botaoDeleta(item.id))
 
     lista.appendChild(novoItem)
 
+
+}
+
+function atualizaElemento(item){
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(id){
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function() {
+        console.log(this)
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id){
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
